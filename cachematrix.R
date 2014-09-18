f7b64c4715dacdb216a4a0679e67cd843b3e953e
Georgiy <gkassabli@Georgiys-MacBook-Pro-2.local>
2014-09-18 16:06:38 +0300

    Matrix inverting implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a 'matrix' that is a list containing functions for caching inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedinverse <- NULL
+    set <- function(y) {
+        x <<- y
+        cachedinverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) cachedinverse <<- inverse
+    getinverse <- function() cachedinverse
+    list(set = set, get = get,
+    setinverse = setinverse,
+    getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function either returns cached inverse matrix or calculates it and caches inverse, while returning the result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(x)
+    x$setinverse(inverse)
+    inverse
 }
